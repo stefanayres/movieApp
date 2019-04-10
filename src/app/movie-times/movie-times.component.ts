44fd47942d68d859194faf210522c64d7b928508
stefan Ayres <stefan-1990-07@hotmail.com>
2019-04-10 11:35:14 +0100

    add movie times component to display times a movie will be playing near the user

---
+++ src/app/movie-times/movie-times.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-times',
+  templateUrl: './movie-times.component.html',
+  styleUrls: ['./movie-times.component.sass']
+})
+export class MovieTimesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
