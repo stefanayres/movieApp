f3a85a9cd0a937446aabbf3c40880a7e1d31628b
stefan Ayres <stefan-1990-07@hotmail.com>
2019-04-10 22:54:18 +0100

    Add method for retriving data from movie service
    contains showMovies function for retriving a list of movies, contains showmovie function for getting a single movie

--- src/app/service/movies.service.ts
+++ src/app/service/movies.service.ts
@@ -12,10 +12,19 @@ export class MoviesService {
 
   //url = environment.url;
 
-  showmovies() {
-
+  showmovies(location: any) {
+    console.log(location);
     return this.http.get(`http://localhost:8888/cors-proxy/cors-proxy.php/`);
           }
 
+  showmovie(id: number){
+    return this.http.get(`https://www.omdbapi.com/?i=tt${id}&apikey=db203e5e`);
+  }
+
+          // callApi(Longitude: number, Latitude: number){
+          //   const url = `https://api-adresse.data.gouv.fr/reverse/?lon=${Longitude}&lat=${Latitude}`
+          //   //Call API
+          //   console.log(url);
+          // }
 
 }
