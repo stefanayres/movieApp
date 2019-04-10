0d726260bc181f65819b49bb047b27c6bf4be1d8
stefan Ayres <stefan-1990-07@hotmail.com>
2019-04-10 22:43:51 +0100

    add browser location feature to location component, not fully finished some problems with passing lat and long in url
    Able to ask user for location and will get location and display in innerHTML but unable to use the data in url or routerLink to send to the movieGlu api, need to fix this and send to api and add drop-down list of locations for a user to pick from if not willing to give location. moving on with other features while stuck.

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute } from '@angular/router';
+import { LocationService } from '../service/location.service';
 
 @Component({
   selector: 'app-location',
@@ -7,10 +9,73 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./location.component.sass']
 })
 export class LocationComponent implements OnInit {
+  location: any;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router,
+    private route : ActivatedRoute
+    //private location: LocationService
+  ) { }
 
   ngOnInit() {
+  if (!("geolocation" in navigator)) {
+    alert('Your device cannot be used to find your location...');
+  }
+console.log(this.location);
+
+  const getLocationLink     = document.querySelector('#getLocation');
+  const manualLocationInput = document.querySelector('#manualLocation');
+  const feedbackDiv         = document.querySelector('#feedback');
+
+  function getPositionSuccess(pos, location: any){
+
+    console.log(pos);
+
+    const latitude  = pos.coords.latitude;
+    const longitude = pos.coords.longitude;
+    location = latitude + ';' + longitude;
+
+    //console.log(location);
+
+    feedbackDiv.innerHTML = `
+      <p><strong>Latitude:</strong> ${latitude}<br>
+      <strong>Longitude:</strong> ${longitude}<br>
+
+      <div>
+        <a [routerLink]='/listMovies/${location}'>Find Movies Near Me - ${location}</a>
+      </div>
+    `;
   }
 
+  function getPositionFailure(_err: any) {
+    feedbackDiv.innerHTML = 'Error retrieving location.';
+  }
+
+  getLocationLink
+    .addEventListener('click', _e => {
+      feedbackDiv.innerHTML = 'Looking...';
+      navigator
+        .geolocation
+        .getCurrentPosition(
+          getPositionSuccess,
+          getPositionFailure
+        );
+
+    });
+
+  manualLocationInput
+    .addEventListener('change', () => {
+      feedbackDiv.innerHTML = 'Looking...';
+      // navigator
+      //   .geolocation
+      //   .getCurrentPosition(
+      //     getPositionSuccess,
+      //     getPositionFailure
+      //   );
+    });
+
 }
+
+
+} // end of ngOnInIt
