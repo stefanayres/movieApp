bc542e76488a6413543c6cd8c4d9c16f7235a7ad
stefan Ayres <stefan-1990-07@hotmail.com>
2019-04-14 00:22:52 +0100

    add getGeo function
    this was a test to see if the global service worked, it calls the getGeo function in the constructor and logs it in the ngOnInit method.

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MoviesService } from '../service/movies.service';
  //https://medium.com/@christo8989/angular-6-url-parameters-860db789db85
- import { NgxSpinnerService } from 'ngx-spinner';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { GlobalService } from '../global.service';
 
 @Component({
   selector: 'app-movies',
@@ -12,34 +13,31 @@ import { MoviesService } from '../service/movies.service';
 export class MoviesComponent implements OnInit {
     movies: any;
     data: any;
+    private geo: any = null;
 
   constructor(
     private spinner: NgxSpinnerService,
     private service: MoviesService,
     private router: Router,
-    private route : ActivatedRoute
-  ) { }
+    private route : ActivatedRoute,
+    private Global: GlobalService
+  ) {
+    this.geo = this.Global.getGeo();
+  }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
     this.data = params.get("location")
     })
 
+    console.log(this.geo);
     this.spinner.show();
     this.service.showmovies(this.data)
         .subscribe(data => {
           this.movies = data as any;
-          console.log(this.movies);
-           //this.spinner.hide();
            setTimeout(() => {
                    this.spinner.hide();
                }, 900);
         });
       };
-
-
-
-
-
-
-}
+  }
