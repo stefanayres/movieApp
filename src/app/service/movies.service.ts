1807f6256be997b42fe2b24df6146fac66b7d587
stefan Ayres <stefan-1990-07@hotmail.com>
2019-04-14 00:25:38 +0100

    add the global service for getGeo function
    testing the global service, calls the getGeo function the constructor and logs the result in the ngOnInit method

--- src/app/service/movies.service.ts
+++ src/app/service/movies.service.ts
@@ -1,34 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-//import { environment } from '../../environments/environment';
+import { GlobalService } from '../global.service';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
+  private location: any = null;
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private Global: GlobalService
+  ) {
+    this.location = this.Global.getGeo();
+   }
 
-  //url = environment.url;
+  omdbApi: any = 'db203e5e';
+  moviebdApi: any = '7b368fb69167a2d40c7c86bd6a293029';
 
   showmovies(location: any) {
     console.log(location);
-    return this.http.get(`http://localhost:8888/cors-proxy/cors-proxy.php/`);
-          }
+    return this.http.get(`http://localhost:8888/cors-proxy/cors-proxy.php?loc=${location}`);
+  }
 
   showmovie(id: number){
-    return this.http.get(`https://www.omdbapi.com/?i=tt${id}&apikey=db203e5e`);
+    return this.http.get(`https://www.omdbapi.com/?i=tt${id}&apikey=${this.omdbApi}`);
   }
 
   getTrailer(id: any){
-    return this.http.get(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US&api_key=7b368fb69167a2d40c7c86bd6a293029`);
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US&api_key=${this.moviebdApi}`);
   }
 
-          // callApi(Longitude: number, Latitude: number){
-          //   const url = `https://api-adresse.data.gouv.fr/reverse/?lon=${Longitude}&lat=${Latitude}`
-          //   //Call API
-          //   console.log(url);
-          // }
 
 }
