22dc16da3fc6b67d348a290446cab93935ea5c0c
stefan Ayres <stefan-1990-07@hotmail.com>
2019-04-14 00:12:14 +0100

    add global var code
    set up global service import and created set global var function, then set the global geo variable to the long and lat got from the geolocation.getCurrentPostion function.

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router, ActivatedRoute } from '@angular/router';
+import { GlobalService } from '../global.service'; // test gloabal var
 
 @Component({
   selector: 'app-location',
@@ -11,9 +12,10 @@ export class LocationComponent implements OnInit {
 
 lat: any;
 long: any;
-
+geo: any; // test gloabal var
 
   constructor(
+    private Global: GlobalService, // test gloabal var
     private httpClient: HttpClient,
     private router: Router,
     private route : ActivatedRoute
@@ -25,17 +27,28 @@ long: any;
     alert('Your device cannot be used to find your location...');
   }
 
+
   const getLocationLink     = document.querySelector('#getLocation');
   const feedbackDiv         = document.querySelector('#feedback');
+  const locationServiceOpts = {enableHighAccuracy: true}
 
   function getPositionSuccess(pos){
+    console.log(pos);
 
-    this.lat = pos.coords.latitude;
+    this.lat = pos.coords.latitude; // error here, wont pass this point
     this.long = pos.coords.longitude;
+
+    const latLocal  = pos.coords.latitude;
+    const longLocal = pos.coords.longitude;
+
+    //this.lat = latLocal;
+    //this.long = longLocal;
+
+    console.log(latLocal); // will not show now.
+
     console.log(this.lat);
     console.log(this.long);
-
-
+    this.geo = this.lat, this.long;   // test global var
   }
 
   function getPositionFailure(_err: any) {
@@ -47,27 +60,34 @@ long: any;
       feedbackDiv.innerHTML = 'Looking...';
       navigator
         .geolocation
-        .getCurrentPosition(
-          getPositionSuccess,
-          getPositionFailure
+          .getCurrentPosition(
+            getPositionSuccess,
+              getPositionFailure,
+              locationServiceOpts
         );
 
     });
 
   }// end of ngOnInit
 
+  SetGlobalData() { // set the geo var to global var for use else where. 
+        this.Global.setGeo(this.geo);
+    }
+
   cities = [{'name':'Dundalk','lat': '54.003927', 'long': '-6.402207'}, //54.003927, -6.402207
             {'name':'-None-','lat': '0', 'long': '0'},
             {'name':'Droghada','lat': '53.714452', 'long': '-6.351818'},//53.714452, -6.351818
             {'name':'Cavan','lat': '53.990751', 'long': '-7.362873'},//53.990751, -7.362873
             {'name':'Galway','lat': '53.270695', 'long': '-9.061707'},//53.270695, -9.061707
-            {'name':'Cork','lat': '51.897617', 'long': 'te-8.473983'}, //51.897617, -8.473983
+            {'name':'Cork','lat': '51.897617', 'long': '-8.473983'}, //51.897617, -8.473983
             {'name':'Dublin','lat': '53.346293', 'long': '-6.275564'}]; //53.346293, -6.275564
   selectedCity = this.cities[1];
 
   onChange(city: any) {
     this.lat = city.lat;
     this.long = city.long;
+
+    this.geo = this.lat + this.long; // saving the data to global var
   }
 
 
