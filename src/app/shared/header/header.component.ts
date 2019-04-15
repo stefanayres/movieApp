e69ae9c9b638129773eece5b2fabc5570746fb7f
stefan Ayres <stefan-1990-07@hotmail.com>
2019-04-15 22:06:38 +0100

    fix back button and style the nav bar for mobile and desktop
    back button using the angular location back() function. also cleaned the code and styled nav while adding the app logo

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import {Location} from '@angular/common';
 
 
@@ -8,20 +9,29 @@ import {Location} from '@angular/common';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-    title = 'Movies Near You';
-    geo: any;
+  title = 'Movies Near You';
+  isRoot: boolean;
 
-    pageLinks = [
-      {path: '/',           name: 'Home'},
-      {path: '/listMovies/:location',   name: 'List Movies'},
-      { path: '**', redirectTo: '' }
-    ];
+  pageLinks = [
+    {path: '/',           name: 'Home'},
+    { path: '**', redirectTo: '' }
+  ];
 
-  constructor(private _location: Location) { }
+  constructor(
+    private _location: Location,
+    private router: Router,
+    private route : ActivatedRoute
+    ) { }
 
   ngOnInit() {
-
-  }
+      this.router.events.subscribe(event => {
+      if (this._location.path() !== '') {
+        this.isRoot = false;
+      } else {
+        this.isRoot = true;
+      }
+    });
+   }
 
   backClicked() {
     this._location.back();
