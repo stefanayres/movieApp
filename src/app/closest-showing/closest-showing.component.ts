08b469fcf41e58483f74411e79c311feca871bbe
stefan Ayres <stefan-1990-07@hotmail.com>
2019-04-15 02:51:56 +0100

    add closest showing components
    these components will show the cinemas and closest showing times of a move based on film id and geo location. each result is styled like the movies list cards.

--- src/app/closest-showing/closest-showing.component.ts
+++ src/app/closest-showing/closest-showing.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MoviesService } from '../service/movies.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-closest-showing',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./closest-showing.component.scss']
 })
 export class ClosestShowingComponent implements OnInit {
+  //data: any;
+  cinemas: any;
+  lat: any;
+  lon: any;
+  filmId: any;
 
-  constructor() { }
+  constructor(
+    private spinner: NgxSpinnerService,
+    private service: MoviesService,
+    private router: Router,
+    private route : ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.lat    = sessionStorage.lat;
+    this.lon    = sessionStorage.long;
+    this.filmId = sessionStorage.film_id;
+
+    console.log(this.filmId);
+    // this.route.paramMap.subscribe(params => {
+    // this.data = params.get("location")
+    // })
+
+    this.spinner.show();
+    this.service.closeShowings(this.lat, this.lon, this.filmId)
+        .subscribe(data => {
+          this.cinemas = data as any;
+          console.log(this.cinemas)
+           setTimeout(() => {
+                   this.spinner.hide();
+               }, 900);
+        });
   }
 
 }
