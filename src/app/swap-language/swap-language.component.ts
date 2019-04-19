39434182771a67b72c045eb56613e6a2aab94d2f
stefan Ayres <stefan-1990-07@hotmail.com>
2019-04-19 22:42:03 +0100

    add styling to lang dropdown and clean code

--- src/app/swap-language/swap-language.component.ts
+++ src/app/swap-language/swap-language.component.ts
@@ -5,7 +5,7 @@ import {TranslateService} from '@ngx-translate/core';
 @Component({
   selector: 'app-swap-language',
   templateUrl: './swap-language.component.html',
-  styleUrls: ['./swap-language.component.sass']
+  styleUrls: ['./swap-language.component.scss']
 })
 export class SwapLanguageComponent implements OnInit {
 
