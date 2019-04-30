07acf5929c3e0acfe840577bd8d36344b3719a08
stefan Ayres <stefan-1990-07@hotmail.com>
2019-04-30 16:58:41 +0100

    add code and styling for sticky nav bar
    before the user had to scroll back up to the top of the page to change location or use the back button. this was not good user expereance so code found on joeljoseph.net was added modified to make the header nav stick to the top.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 import { Router, ActivatedRoute } from '@angular/router';
 import {Location} from '@angular/common';
+import { DOCUMENT } from '@angular/common';
 
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations:[
+    trigger('fade',
+    [
+      state('void', style({ opacity : 0})),
+      transition(':enter',[ animate(300)]),
+      transition(':leave',[ animate(500)]),
+    ]
+  )]
 })
 export class HeaderComponent implements OnInit {
   title = 'Movies Near You';
   isRoot: boolean;
+
 
   pageLinks = [
     {path: '/',           name: 'Home'},
@@ -18,6 +29,7 @@ export class HeaderComponent implements OnInit {
   ];
 
   constructor(
+    @Inject(DOCUMENT) document,
     private _location: Location,
     private router: Router,
     private route : ActivatedRoute
@@ -31,6 +43,17 @@ export class HeaderComponent implements OnInit {
         this.isRoot = true;
       }
     });
+   }
+// code from http://joeljoseph.net/angular-sticky-header-on-scroll/ for sticky nav bar
+   @HostListener('window:scroll', ['$event'])
+   onWindowScroll(e) {
+      if (window.pageYOffset > 0) {
+        let element = document.getElementById('navbar');
+        element.classList.add('sticky');
+      } else {
+       let element = document.getElementById('navbar');
+         element.classList.remove('sticky');
+      }
    }
 
   backClicked() {
