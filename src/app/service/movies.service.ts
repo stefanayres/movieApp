76e28e0e75194d046abe6f8846fec6f35942adbe
stefan Ayres <stefan-1990-07@hotmail.com>
2019-05-03 22:03:55 +0100

    add getposter() function
    getPoster function will retrive a list of trending movies with image urls that will be displayed on the location page.

--- src/app/service/movies.service.ts
+++ src/app/service/movies.service.ts
@@ -30,6 +30,10 @@ export class MoviesService {
     return this.http.get(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US&api_key=${this.moviebdApi}`);
   }
 
+  getPoster(){
+    return this.http.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${this.moviebdApi}`);
+  }
+
   closeShowings(lat: any, lon: any, id: any){
     const  params = new  HttpParams({fromString:  'lat='+lat+'&lon='+lon+'&id='+id+''});
     return this.http.get(`http://localhost:8888/cors-proxy/cors-proxy-cinemas.php` // https://www.stefandesigns.org/cors/cors-proxy-cinemas.php
